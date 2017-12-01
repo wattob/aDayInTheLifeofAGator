2d1614fe5d9eb6ce736718215b60341cbdcba56f
Dillon Thoma <thomad@aldenv187.allegheny.edu>
2017-11-30 19:10:09 -0500

    One working pathway

--- src/main/java/labeleven/InteractiveGameMain.java
+++ src/main/java/labeleven/InteractiveGameMain.java
@@ -8,42 +8,42 @@ public class InteractiveGameMain {
     Scanner scan = new Scanner(System.in);
 
     String Decision1, Decision2, Decision3, Decision4, Decision5;
-    System.out.println();
+
     System.out.println("Welcome to Interactive Storytelling:");
     System.out.println();
     System.out.println("A Day In the Life of a Gator!");
     System.out.println();
     System.out.println("You wake up to the buzzing of your alarm. \nDo you go back to 'sleep' or 'get up' out of bed?");
-    Decision1 = scan.next();
+    Decision1 = scan.nextLine();
 
     if (Decision1.equalsIgnoreCase("get up")) {
-        System.out.println("You get out of bed and get into a nice, lukewarm shower.");
-        System.out.println("On the way to class you see a monster. \nDo you 'kick' or 'pet' it?");
-        Decision2 = scan.next();
+        System.out.println();
+        System.out.println("you do stuff 'stuff' yay or yeah the other 'thing' man");
+        Decision2 = scan.nextLine();
 
-      if(Decision2.equalsIgnoreCase("pet")) {
-        System.out.println("You were bitten! Now you need a rabies shot.");
-        System.out.println("Game Over!")
-    }
-      } else if (Decision2.equalsIgnoreCase("kick")) {
-          System.out.println();
-          System.out.println("You dang monster you! Good work protecting the campus.");
-          System.out.println();
-          System.out.println("You now walk into Brooks to get yourself some lunch. \nDo you get food from the 'main line' or head straight to 'slice'?");
-          Decision3 = scan.next();
-
-          if (Decision3.equalsIgnoreCase("main line")) {
+        if (Decision2.equalsIgnoreCase("stuff")) {
             System.out.println();
-            System.out.println("You get food poisining!")
-            System.out.println("Game Over!");
-        //  } else if (Decision4.equalsIgnoreCase())
-       }
-    } else if (Decision1.equalsIgnoreCase("sleep")) {
-      System.out.println("You sleep through all of your classes and fail out. Nice work!");
-      System.out.println("Game Over!");
-        Decision2 = scan.next();
+            System.out.println(" wow theres stuff \nWould you like to eat some of the food, 'Yes' or 'No'?");
+            Decision3 = scan.nextLine();
 
+            if (Decision3.equalsIgnoreCase("Yes")) {
+                System.out.println();
+                System.out.println("You live!");
+            } else if (Decision3.equalsIgnoreCase("No")) {
+                System.out.println();
+                System.out.println("You lose!");
+              }
+        } else if (Decision2.equalsIgnoreCase("thing")) {
+            System.out.println("There is a puppy inside. Do you want to 'fight' it, or 'run away'?");
+            System.out.println();
+            Decision3 = scan.nextLine();
 
+            if (Decision3.equalsIgnoreCase("fight")) {
+                System.out.println();
+                System.out.println("You did it!");
+            } else if (Decision3.equalsIgnoreCase("run away")) {
+                System.out.println();
+                System.out.println("the puppy is too fast and you died");
       }
     }
   }
