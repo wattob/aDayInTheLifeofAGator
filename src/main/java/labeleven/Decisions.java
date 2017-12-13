5065fadfc577d125534f71ce4b2213423d77e42b
Dillon Thoma <thomad@aldenv139.allegheny.edu>
2017-12-13 13:12:47 -0500

    git adding other files

---
+++ src/main/java/labeleven/Decisions.java
@@ -0,0 +1,65 @@
+package labeleven;
+
+import java.util.*;
+
+/** The source code will .
+ *
+ *
+ *
+ */
+
+public class Decisions {
+  String decision1;
+  String decision2;
+  String decision3;
+  String decision4;
+  String decision5;
+  String decision7;
+
+  void addDecisions(String d1, String d2, String d3, String d4, String d5, String d7){
+    decision1 = d1;
+    decision2 = d2;
+    decision3 = d3;
+    decision4 = d4;
+    decision5 = d5;
+    decision7 = d7;
+  }
+
+  void printDecisions(){
+    System.out.println(decision1);
+    System.out.println(decision2);
+    System.out.println(decision3);
+    System.out.println(decision4);
+    System.out.println(decision5);
+    System.out.println(decision7);
+  }
+
+}
+// public class InteractiveGameList{
+//       ArrayList<String> decision = new ArrayList<String>();
+//
+//
+//       decision.add("Decision1");
+//       decision.add("Decision2");
+//       decision.add("Decision3");
+//       decision.add("Decision4");
+//       decision.add("Decision5");
+//       decision.add("Decision7");
+//
+//
+//       System.out.println("Currently the decisions have been the following:" + decision);
+//
+//       decision.add(0, "sleep");
+//       decision.add(1, "food");
+      //decision.remove("");
+      //decision.remove("");
+
+      //System.out.println("Current array list is:" + decision);
+      //decision.remove(0);
+      //System.out.println("Current array list is:" + decision);
+
+// //  add comments
+// //  type reflection and other writing
+// //  add fight
+// //  array list
+// //  make 3 classes
